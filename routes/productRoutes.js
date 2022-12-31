4be7cdd720eeecdc3f40b4a385527e2c59b70fb9
matthewhiggins415 <mhiggins.louis@gmail.com>
2022-12-30 17:51:16 -0800

    added delete route for products.

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -82,7 +82,19 @@ router.patch('/products/:id', async (req, res, next) => {
   }
 })
 
-
 // delete 
+router.delete('/products/:id', async (req, res, next) => {
+  let id = req.params.id
+
+  try {
+    await Product.findByIdAndDelete(id)
+    let products = await Product.find()
+    res.json({ products })
+  } catch (error) {
+    console.error(`Error msg: ${error.message}`)
+  }
+})
+
+
 
 module.exports = router
