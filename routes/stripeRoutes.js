ead247ee5305c59f53b2a406894e61ce2c2bee4a
matthewhiggins415 <mhiggins.louis@gmail.com>
2023-01-13 16:31:43 -0700

    added stripe checkout session to our checkout route.

--- routes/stripeRoutes.js
+++ routes/stripeRoutes.js
@@ -1,13 +1,63 @@
 const express = require('express')
 const router = express.Router()
 
-const stripe = require('stripe')('pk_test_51MPF6BGMgQtLI79Vs1hM0pOZClykWig8rLLlhsEkv8IkQ26GBCCVPWW91wbkTqefhk8dDkwRdCA3JtIzIXjCGn6x00OACyHmWr')
+const stripe = require('stripe')('sk_test_51MPF6BGMgQtLI79Vke2qA5psRAQL3uChje6RF3xAyCQgkZd5tOMP9NoHaMdvNy9rgQg5xQYF1xV36P26HyUgIKmf005sjvEhQZ');
 
 const Product = require('../models/productSchema')
 
 router.post('/checkout', async (req, res) => {
   const { cart } = req.body
   console.log(cart)
+
+  let lineItems = []
+
+  // line_item: {
+//       price_data: {
+//         currency: 'usd', 
+//         product_data: {
+//         name: "name", 
+//         images: ["src"]
+//       }, 
+//       unit_amount: price * 100,
+//      }, 
+//       quantity: 2
+//  }
+
+  for (const item of cart) {
+    console.log(item)
+    const { id, quantity } = item
+    let product = await Product.findById({_id: id})
+    console.log(product)
+    const { name, price, imageOne } = product
+
+    const lineItem = {
+      price_data: {
+        currency: 'usd', 
+        product_data: {
+          name: name,
+          images: [imageOne]
+        },
+        unit_amount: price * 100
+      },
+      quantity: quantity
+    }
+
+    lineItems.push(lineItem)
+
+
+  }
+
+  console.log(lineItems)
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: lineItems, 
+    mode: 'payment', 
+    success_url: 'http://localhost:3000/success',
+    cancel_url: 'http://localhost:3000/cancel'
+  })
+
+  res.json({ "url": session.url })
+
 })
 
 module.exports = router
