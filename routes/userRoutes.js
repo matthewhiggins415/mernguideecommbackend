6b00f25c3842044e012f01e7be7e2cfea48f5ba5
matthewhiggins415 <mhiggins.louis@gmail.com>
2023-01-31 14:34:37 -0700

    added user login route.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -25,8 +25,33 @@ router.post('/register', async (req, res, next) => {
     hashedPassword: hash
   }
 
+
   let user = await User.create(userObj)
   
+  const token = crypto.randomBytes(16).toString('hex')
+  user.token = token 
+  await user.save()
+
+  res.json({ user: user })
+})
+
+//login route 
+router.post('/login', async (req, res, next) => {
+  const { credentials } = req.body
+  const { email, password } = credentials
+
+  let user = await User.findOne({ email: email })
+
+  if (!user) {
+    res.json({ msg: "Login Failed" })
+  }
+
+  let correctPassword = await bcrypt.compare(password, user.hashedPassword)
+
+  if (!correctPassword) {
+    res.json({ msg: "Login Failed" })
+  }
+
   const token = crypto.randomBytes(16).toString('hex')
   user.token = token 
   await user.save()
